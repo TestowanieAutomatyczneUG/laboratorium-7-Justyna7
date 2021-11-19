6a60ca098ba7957db19de44631682f3f66ecb56e
Justyna <justiluc@interia.pl>
2021-11-19 22:02:43 +0100

    lab07 zad 1 poprawki

--- lab07zad1/main.py
+++ lab07zad1/main.py
@@ -1,17 +1,21 @@
 class FizzBuzz:
-    def game(self, num):
-        fb = ""
-        if type(num) == int:
-            if num % 5 == 0 or num % 3 == 0:
-                if num % 3 == 0:
-                    fb = fb + "Fizz"
-                if num % 5 == 0:
-                    fb = fb + "Buzz"
-                return fb
+    def game(self, *args):
+        if len(args) == 1:
+            num = args[0]
+            fb = ""
+            if type(num) == int:
+                if num % 5 == 0 or num % 3 == 0:
+                    if num % 3 == 0:
+                        fb = fb + "Fizz"
+                    if num % 5 == 0:
+                        fb = fb + "Buzz"
+                    return fb
+                else:
+                    raise Exception("Error in FizzBuzz")
             else:
                 raise Exception("Error in FizzBuzz")
         else:
-            raise Exception("Error in FizzBuzz")
+            raise ValueError("Wrong number of arguments")
 
     def gameWithDocString(self, x):
         """Returnes Fizz when n is divisible by 3, Buzz when n is divisible by 5 and FizzBuzz when n is divisible by 15
@@ -57,20 +61,23 @@ class FizzBuzz:
             exec(compile(example.source, filename, "single",
           File "<doctest main.FizzBuzz.gameWithDocString[8]>", line 1, in <module>
             f.game()
-        TypeError: FizzBuzz.game() missing 1 required positional argument: 'num'
+          File "D:/uczelnia/II rok/semestr 3/testowanie_automatyczne/lab07/laboratorium-7-Justyna7/lab07zad1/main.py", line 18, in game
+            raise ValueError("Wrong number of arguments")
+        ValueError: Wrong number of arguments
         >>> f.game(16, 4)
         Traceback (most recent call last):
           File "C:\Program Files\JetBrains\PyCharm Community Edition 2021.2.3\plugins\python-ce\helpers\pycharm\docrunner.py", line 138, in __run
             exec(compile(example.source, filename, "single",
           File "<doctest main.FizzBuzz.gameWithDocString[9]>", line 1, in <module>
             f.game(16, 4)
-        TypeError: FizzBuzz.game() takes 2 positional arguments but 3 were given
+          File "D:/uczelnia/II rok/semestr 3/testowanie_automatyczne/lab07/laboratorium-7-Justyna7/lab07zad1/main.py", line 18, in game
+            raise ValueError("Wrong number of arguments")
+        ValueError: Wrong number of arguments
         """
-
 
 
 if __name__ == '__main__':
     import doctest
     doctest.testmod(extraglobs={'f': FizzBuzz()})
 
-#Odpalenie: python3 main.py -v
+# Odpalenie: python3 main.py -v
