9ac2d7b0a7e984063b4a5df31381c08e85949693
Justyna <justiluc@interia.pl>
2021-11-23 22:49:48 +0100

    drobne poprawki

--- lab07zad2/tests/testPasswordDocTest.py
+++ lab07zad2/tests/testPasswordDocTest.py
@@ -5,7 +5,7 @@ from main import *
 
 class testPasswordDoc:
     def testValidPasswordWithDocString(self):
-        """Returnes Fizz when n is divisible by 3, Buzz when n is divisible by 5 and FizzBuzz when n is divisible by 15
+        """Returns True if Pass is >=8 characters long, has atl least one lowercase letter, uppercase letter, number and other nonwhite character
         >>> f = Password()
         >>> f.ValidPassword(1)
         False
